db5f0cd96c2fba65f18e3ffb77db84cc9d9dcd67
Anut.pri <124768336+anutpri@users.noreply.github.com>
2023-03-15 14:39:29 +0000

    finish Exercise1.js

---
+++ Exercise1.js
@@ -0,0 +1,12 @@
+// Part 1
+const customer = ['Sofia', 'David', 'Juan'];
+
+// Part 2
+customer.push('Sara', 'Augustin');
+customer.shift(0);
+
+// Part 3
+customer.splice(1,0,'Renata');
+customer.push('Elena');
+
+console.log(customer);
